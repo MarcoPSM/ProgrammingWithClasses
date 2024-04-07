0eaa5d0ed3e5de5a8b88246cb926ae83b54b9406
Marco Martins <marco.martins@nokia.com>
2024-04-07 23:19:10 +0000

    Small changes on PersonTestSuite and NaiveDateTestSuite

--- tests/NaiveDateTest.cpp
+++ tests/NaiveDateTest.cpp
@@ -2,22 +2,22 @@
 // Created by marco on 11-03-2023.
 //
 
-#include "gtest/gtest.h"
 #include "NaiveDate.h"
+#include "gtest/gtest.h"
 
 TEST(NaiveDateTestSuite, Friday13Version0) {
     mar::NaiveDate d;
-    int count = 0;
-    for(;;) {
-        if (d.Day()==13 && d.Weekday()==mar::NaiveDate::FRIDAY ) {
+    int            count = 0;
+    for (;;) {
+        std::cout << "Count: " << count << std::endl;
+        if (d.Day() == 13 && d.Weekday() == mar::NaiveDate::FRIDAY) {
             count++;
-            //d.WriteLine();
-            if (count==13) {
+            d.WriteLine();
+            if (count == 1) {
                 break;
             }
         }
         d.Forth();
     }
-    EXPECT_EQ(count, 13);
+    EXPECT_EQ(count, 1);
 }
-
--- tests/models/PersonTest.cpp
+++ tests/models/PersonTest.cpp
@@ -2,20 +2,19 @@
 // Created by Marco Martins on 04-06-2023.
 //
 
-
-#include "gtest/gtest.h"
-#include "ThermometerSimple.h"
-#include "ThermometerExtreme.h"
-#include "ThermometerMulti.h"
-#include "Thermograph.h"
 #include "Person.h"
 #include "Friend.h"
+#include "Thermograph.h"
+#include "ThermometerExtreme.h"
+#include "ThermometerMulti.h"
+#include "ThermometerSimple.h"
+#include "gtest/gtest.h"
 
 TEST(PersonTestSuite, person) {
     mar::Person p1("Carla", mar::Person::FEMALE);
     p1.AddContact("carla@mail.com");
     mar::Person p2(p1);
-    std::cout << p1 << std::endl;
+    std::cout << "Person: " << p1 << std::endl;
     EXPECT_NE(p1, p2);
 }
 
@@ -25,6 +24,6 @@ TEST(PersonTestSuite, friend) {
     f1.AddContact("marco@mail.com");
     f1.RaiseLevel(5);
     f1.SetBirthdate(mar::Date(1980, 07, 25));
-    //f1.WriteLine();
+    // f1.WriteLine();
     EXPECT_EQ(17, f1.Level());
 }
